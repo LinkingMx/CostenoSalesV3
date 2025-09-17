1981b28d136b0dca618f4e08f6d37ce7d4480051
LinkingMx <armando.reyes@live.com.mx>
2025-09-17 09:12:23 -0600

    Cambio en el logo de app
    Logo de app en el login

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,4 +1,4 @@
-import AppLogoIcon from '@/components/app-logo-icon';
+import AppLogo from '@/components/app-logo';
 import { home } from '@/routes';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
@@ -16,8 +16,8 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={home()} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <div className="mb-1 flex h-24 w-auto items-center justify-center">
+                                <img src="/logo_costeno_gold.svg" alt="Costeno Sales" className="h-12 w-auto object-contain" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
